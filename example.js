201047138645c866b2c9e91766e1e2316d3eb4ec
Assaf Suberry <87972790+Assafsu@users.noreply.github.com>
2021-09-02 19:48:35 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/Assafsu/iat_test@main/example2.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 		//hello world
 	return iatExtension({
